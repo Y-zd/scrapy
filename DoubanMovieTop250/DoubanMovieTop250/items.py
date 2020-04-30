907ace7f15fe82caaac9dccd9e97c218b9ff0322
yanzhidong <yanzhidong@sm.vvip-u.com>
2020-04-30 15:45:52 +0800

    init

---
+++ DoubanMovieTop250/DoubanMovieTop250/items.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class DoubanMovieItem(scrapy.Item):
+
+    ranking = scrapy.Field()
+
+    movie_name = scrapy.Field()
+
+    score = scrapy.Field()
+    
+    score_num = scrapy.Field()
